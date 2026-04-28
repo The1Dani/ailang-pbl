fba5777b2b07f450e469136d82106ff45d37167d
The1Dani <daniel.canter2013@gmail.com>
2026-04-28 23:07:28 +0300

    refactoring print function to be compliant with new kwarg support

--- AiLangLib.py
+++ AiLangLib.py
@@ -1,35 +1,60 @@
 import sys
+import codecs
 from AiLangFunc import makeFunc, makeMethod
 from AiLangObj import AiLangObj, NoneObj
-from AiLangType import AiLangType, NumType, NumTypes
+from AiLangType import NumType, NumTypes
 import AiLangBuiltinDfLib as _
 
 
+def toStringDecoded(obj: AiLangObj) -> str:
+    val = obj.get().get()
+    return codecs.decode(str(val), "unicode-escape")
+
+
 @makeFunc("print")
-def aiLangBuiltinPrint(*items: AiLangObj | AiLangType) -> AiLangObj:
+def aiLangBuiltinPrint(*args, **kwargs) -> AiLangObj:
 
-    for item in items:
-        if isinstance(item, AiLangObj):
-            item = item.get()
+    args = list(args)
 
-        val = item.get()
-        print(val)
+    sep, end = None, None
+
+    if "sep" in kwargs:
+        sep = toStringDecoded(kwargs["sep"])
+        del kwargs["sep"]
+
+    if "end" in kwargs:
+        end = toStringDecoded(kwargs["end"])
+        del kwargs["end"]
+
+    kwarg_vals = list(kwargs.values())
+    args += kwarg_vals
+
+    print(f"DEBUG: {args}")
+
+    values = [toStringDecoded(val) for val in args]
+
+    print(*values, sep=sep, end=end)
 
     return NoneObj()
 
 
 @makeFunc("exit")
-def aiLangBuiltinExit(*items):
+def aiLangBuiltinExit(*items: AiLangObj):
     ret_code = None
     if len(items) > 0:
         first = items[0]
-        if isinstance(first.get(), NumType) and first.get().type == NumTypes.INT:
-            ret_code = first.get().get()
+        first_type = first.get()
+        if isinstance(first_type, NumType):
+            if first_type.type == NumTypes.INT:
+                ret_code = first_type.get()
 
     ret_code = ret_code if ret_code else 0
     sys.exit(ret_code)
 
 
 @makeMethod("rest", type(None))
-def aiLangBuiltinDFRest(parent, *args) -> AiLangObj:  # pylint: disable=unused-argument
+def aiLangBuiltinDFRest(
+    parent, *args, **kwargs
+) -> AiLangObj:  # pylint: disable=unused-argument
+    print(f"[DEBUG] {parent=} {args=} {kwargs=}")
     return AiLangObj("")
